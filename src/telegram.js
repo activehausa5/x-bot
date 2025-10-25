03a1eda7013489828ea7db0b14ba9f60fa2ff166
activehausa5 <activehausa@proton.me>
2025-10-25 19:39:23 +0000

    Update telegram.js
    update it

--- src/telegram.js
+++ src/telegram.js
@@ -4,24 +4,23 @@ const config = require("./config");
 const logger = require("./logger");
 
 async function sendTelegramMessage(message) {
-  const token = process.env.TELEGRAM_BOT_TOKEN;
-  const chatId = process.env.TELEGRAM_CHAT_ID;
+   if (!config.telegram.botToken || !config.telegram.chatId) return;
 
-  if (!token || !chatId) {
-    logger.error("Telegram not configured: missing token or chat ID");
-    return;
-  }
-
-  const url = `https://api.telegram.org/bot${token}/sendMessage`;
+  const url = `https://api.telegram.org/bot${config.telegram.botToken}/sendMessage`;
   try {
     await axios.post(url, {
-      chat_id: chatId,
+      chat_id: config.telegram.chatId,
       text: message,
       parse_mode: "Markdown",
     });
     logger.info("Telegram notification sent successfully.");
   } catch (error) {
     logger.error(
+      `Error sending Telegram message: ${error.message}, ${JSON.stringify(
+        error.response?.data || {}
+      )}`
+    );
+   console.error(
       `Error sending Telegram message: ${error.message}, ${JSON.stringify(
         error.response?.data || {}
       )}`
